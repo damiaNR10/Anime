5802e618af837201108a2ebad8dc841c9988f372
damiaNR10 <dymon10@o2.pl>
2018-09-11 16:14:22 +0200

    last commit

--- app/src/main/java/com/example/damia/anime/activities/AnimeActivity.java
+++ app/src/main/java/com/example/damia/anime/activities/AnimeActivity.java
@@ -1,8 +1,13 @@
 package com.example.damia.anime.activities;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.damia.anime.R;
 
 public class AnimeActivity extends AppCompatActivity {
@@ -13,5 +18,36 @@ public class AnimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_anime);
 
         getSupportActionBar().hide();
+
+        String name = getIntent().getExtras().getString("anime_name");
+        String description = getIntent().getExtras().getString("anime_description");
+        String category = getIntent().getExtras().getString("anime_category");
+        String rating = getIntent().getExtras().getString("anime_rating");
+        int nb_episodes = getIntent().getExtras().getInt("anime_nm_episode");
+        String studio = getIntent().getExtras().getString("anime_studio");
+        String img_url = getIntent().getExtras().getString("anime_img");
+
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingToolBar_Id);
+        collapsingToolbarLayout.setTitleEnabled(true);
+
+        TextView tv_name = findViewById(R.id.aa_anime_name);
+        TextView tv_studio = findViewById(R.id.aa_studio);
+        TextView tv_category = findViewById(R.id.aa_category);
+        TextView tv_description = findViewById(R.id.aa_description);
+        TextView tv_rating = findViewById(R.id.aa_rating);
+        ImageView img = findViewById(R.id.aa_thumbnail);
+
+        tv_name.setText(name);
+        tv_studio.setText(studio);
+        tv_category.setText(category);
+        tv_description.setText(description);
+        tv_rating.setText(rating);
+
+        collapsingToolbarLayout.setTitle(name);
+
+        RequestOptions requestOptions = new RequestOptions().centerCrop().placeholder(R.drawable.loading_shape).error(R.drawable.loading_shape);
+
+        Glide.with(this).load(img_url).apply(requestOptions).into(img);
+
     }
 }
